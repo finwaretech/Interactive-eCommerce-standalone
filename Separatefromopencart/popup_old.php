693badf5ddbc2001cc9de00b43b7066d585fc835
finwaretech <vincent@morrisconsultancy.com>
2017-02-08 10:21:33 +0530

    Add files via upload

---
+++ Separatefromopencart/popup_old.php
@@ -0,0 +1,15 @@
+<!-- 3Dwebcommerce Intergration start -->
+<?php
+    $id = $_GET['id'];
+?>
+<script type="text/javascript">
+var id="<?php echo $id; ?>";//id of the product
+</script>
+<script src="js/jquery.min.js"></script>
+<script src="js/script.js"></script>
+<link rel='stylesheet' href='css/style.css' type='text/css'/>
+
+<!-- Image and Button -->
+<img id="animated_image" src="images/Gifs/<?php echo $id; ?>.gif" >
+ <button type="button" id="button_css_for_EasyMath" class="seedemo single_add_to_cart_button button alt" style="margin-left: 5px;">Click to interact</button>
+<!-- 3Dwebcommerce Intergration start -->
